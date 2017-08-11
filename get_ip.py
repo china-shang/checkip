2cec3b59756398e5af2c5ad5f6c3fd9fab8494f3
china-shang <850747813@qq.com>
2017-08-11 09:24:24 +0800

    merge

--- get_ip.py
+++ get_ip.py
@@ -25,33 +25,24 @@ class IpCreator:
         # self.read_from_file()
 
     async def generate_for_scan(self):
-<<<<<<< HEAD
         if(len(self.list) == 0):
             return None
         return self.list.pop(0)
 
-=======
->>>>>>> master
     def scan_ip(self):
         file = open("ip.txt")
         self.list = file.read().split('|')
         self.list.pop()
-<<<<<<< HEAD
         self.list.pop()
         print(self.list)
         # print(self.list)
         file.close()
 
-=======
->>>>>>> master
     def find_ip(self):
         with open("ip_range.txt") as fd:
             self.str = fd.read()
         self.read_from_file()
 
-<<<<<<< HEAD
-=======
->>>>>>> master
     def read_from_file(self):
         for i in self.str.splitlines():
             min, max = i.split('-')
@@ -62,10 +53,7 @@ class IpCreator:
         #print("end read from file")
 
     async def generate(self):
-<<<<<<< HEAD
         index = random.randint(0, len(self.listmax) - 1)
-=======
->>>>>>> master
         min = self.listmin[index].split('.')
         max = self.listmax[index].split('.')
         l = []
@@ -80,8 +68,3 @@ class IpCreator:
             else:
                 ip += "." + str(i)
         return ip
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-
