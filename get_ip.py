78250934519605605d8d685d00146c6d0b5ed0b0
china-shang <850747813@qq.com>
2017-08-31 15:04:44 +0800

    update

--- get_ip.py
+++ get_ip.py
@@ -15,7 +15,18 @@ class ipFactory:
         self.deadLine = self.nowIndex
         self.q.put((self.nowIndex + 1) % self.iprangelen)
         self.generateIP()
+        self.GoodRange = set()
+        self.getFromGood = False
+        self.getGoodRange()
         print("Now Index:%4d" % self.nowIndex)
+
+    def getGoodRange(self):
+        with open("find_log0.txt") as f:
+            s = f.readlines()
+        self.GoodRange = set([int(i.split(":")[0]) for i in s])
+        self._GoodRange = self.GoodRange.copy()
+        if not self.getFromGood:
+            self.GoodRange = set()
 
     def generateIP(self):
         l1 = [[ipaddress.ip_address(ip.split('-')[0]),
@@ -29,15 +40,25 @@ class ipFactory:
 
     def getIp(self):
         while self.nowIp > self.ipList[self.nowIndex][-1]:
-            self.nowIndex = self.q.get()
-            if self.nowIndex == self.deadLine:
-                raise SystemExit
-            self.q.put((self.nowIndex + 1) % self.iprangelen)
+            self.nowIndex = self.increaseIndex()
             self.nowIp = self.ipList[self.nowIndex][0]
             print("Now Index:%4d" % self.nowIndex)
         t = self.nowIp
         self.nowIp += 1
         return str(t)
+
+    def increaseIndex(self):
+        while True:
+            if len(self.GoodRange) > 0:
+                nowIndex = self.GoodRange.pop()
+                return nowIndex
+            else:
+                nowIndex = self.q.get()
+                if nowIndex == self.deadLine:
+                    raise SystemExit
+                self.q.put((nowIndex + 1) % self.iprangelen)
+                if nowIndex not in self._GoodRange:
+                    return nowIndex
 
     def getIndex(self):
         return self.nowIndex
